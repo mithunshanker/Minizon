3bd2826217cbea7cbc32ed8ac03a62ff3b70b975
mithunshanker <87591451+mithunshanker@users.noreply.github.com>
2025-06-24 16:05:49 +0530

    Update settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -126,10 +126,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Redirect users after login/logout/signup
 LOGIN_REDIRECT_URL = '/'
