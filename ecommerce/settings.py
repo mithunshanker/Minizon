bef2363d9992bbdb264d7184f40c1767d3d55655
mithunshanker <87591451+mithunshanker@users.noreply.github.com>
2025-06-24 16:15:05 +0530

    Update settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -128,7 +128,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = []
 
 # Redirect users after login/logout/signup
 LOGIN_REDIRECT_URL = '/'
